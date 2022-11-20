3bfeed0c35cb59ab62267d60d70183b602526dae
Florentin Marechal <marechalflorentin@gmail.com>
2022-11-20 01:58:15 +0100

    besoin tri map

--- index.js
+++ index.js
@@ -1,19 +1,25 @@
 import { readFile } from 'node:fs/promises';
+import fs from 'fs'
 try {
   const filePath = new URL('./wildEncounters.txt', import.meta.url);
   const contents = await readFile(filePath, { encoding: 'utf8' });
-  const splitContent = contents.split('==========')
+  const splitContent = contents.split('Table')
   console.log(splitContent.length);
-
-  const pokemon = "Pikipek";
   const locationArray = [];
+  const pokemon = "Baltoy";
+
   splitContent.forEach(element => {
+    if (element.includes('Map')){
+      locationArray.push(element);
+    }
     if (element.includes(pokemon)){
+      
       locationArray.push(element);
     }
   })
-  locationArray.forEach(element => {
-    console.log(element.toString());
+  fs.writeFile(`${pokemon}.txt`, `${locationArray.toString()}`, function (err) {
+    if (err) return console.log(err);
+    console.log(`${pokemon}.txt: `, `${locationArray.toString()}`);
   });
 } catch (err) {
   console.error(err.message);
